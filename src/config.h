5c187b92b513c1b5cfe6fab13e33841b0811c588
Vectorized <webby1111@hotmail.com>
2025-01-24 21:54:05 +0000

    T

--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 #define VANITY_CONFIG
 
 static int const MAX_ITERATIONS = 1000000000;
-static int const STOP_AFTER_KEYS_FOUND = 1;
+static int const STOP_AFTER_KEYS_FOUND = 100;
 
 // how many times a gpu thread generates a public key in one go
 __device__ const int ATTEMPTS_PER_EXECUTION = 100000;
@@ -12,11 +12,9 @@ __device__ const int MAX_PATTERNS = 10;
 // exact matches at the beginning of the address, letter ? is wildcard
 __device__ static char const *prefixes[] = {
 	"De1eg",
-	"de1egate",
-	"De1egate",
-	"de1egatexyz",
-	"De1egatexyz",
-	"De1egateXYZ"
+	"De1ega",
+	"De1egat",
+	"De1egate"
 };
 
 #endif
